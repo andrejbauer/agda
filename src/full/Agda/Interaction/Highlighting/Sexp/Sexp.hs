7edde1f0edee85b5beaa230c7815431363e94a1d
Andrej Bauer <Andrej.Bauer@andrej.com>
2022-12-08 23:40:05 +0100

    Correctly print special characters in strings, close #2.

--- src/full/Agda/Interaction/Highlighting/Sexp/Sexp.hs
+++ src/full/Agda/Interaction/Highlighting/Sexp/Sexp.hs
@@ -5,6 +5,7 @@ module Agda.Interaction.Highlighting.Sexp.Sexp where
 import Data.Word
 import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy as T
+import qualified Data.Text as DT
 
 data Sexp = Atom Text | String String | Integer Integer | Double Double | Cons [Sexp]
 
@@ -15,7 +16,7 @@ toText :: Sexp -> T.Text
 toText (Atom x)   = x
 toText (Integer k) = T.pack $ show k
 toText (Double x) = T.pack $ show x
-toText (String s) = '"' `T.cons` T.pack s `T.snoc` '"'
+toText (String s) = T.pack $ show s
 toText (Cons lst) = '(' `T.cons` (T.intercalate (T.singleton ' ') (map toText lst)) `T.snoc` ')'
 
 class Sexpable a where
@@ -30,6 +31,12 @@ instance Sexpable Integer where
 
 instance Sexpable String where
     toSexp = String
+
+instance Sexpable T.Text where
+    toSexp t = String $ T.unpack t
+
+instance Sexpable DT.Text where
+    toSexp t = String $ DT.unpack t
 
 instance Sexpable Double where
     toSexp = Double
